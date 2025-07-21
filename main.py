8eb89d5694c1f0090f8366498d8f9545179d26e9
婷琳94524 <tinglin0904@gmail.com>
2025-07-21 11:37:13 +0800

    新增打印"Hello World"

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+print("Hello World!")# This is a simple Python script.
+# It prints "Hello World!" to the console.
 
+https://github.com/
+https://share.streamlit.io/
+https://git-scm.com/downloads/
+https://www.python.org/downloads/
+https://code.visualstudio.com/download
+git config --global user.name "婷琳94524" 
+git config --global user.email "tinglin0904@gmail.com"
